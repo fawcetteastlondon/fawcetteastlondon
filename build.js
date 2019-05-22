5851e39568293b6f49c696e4a9f9d6204ca5f51c
Stephen Tomlinson <Stephen@wcmac31.webcredible.local>
2019-05-22 16:03:56 +0100

    Simple jekyll test site

---
+++ build.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const request = require('request');
+const fs = require('fs');
+const token = 'Bearer ' + process.env.TOKEN;
+
+console.log(process.env);
+
+var options = {
+  url: 'https://www.eventbriteapi.com/v3/organizations/217989441861/events/',
+  accept: 'application/json',
+  headers: {
+    'Authorization': token
+  },
+  qs: {
+    'order_by': 'start_desc'
+  },
+  rejectUnauthorized: false
+};
+var path = './_data/events.json';
+var ws = fs.createWriteStream(path);
+
+// Start the request
+request(options).on('error', function (error) {
+  console.log(error);
+}).on('close', function () {
+  console.log('Done');
+}).pipe(ws);
---
+++ index.md
@@ -0,0 +1,7 @@
+---
+layout: home
+---
+
+We’re the Fawcett Society’s local East London volunteer group. We meet once a month on the first Tuesday between 6.30-8pm and discuss national Fawcett and local level activities as well as hear from external speakers about relevant topics. Come and meet us, everyone’s welcome and we’d love to have you join us!
+
+If you can’t make it to a meeting but want to stay up to date with what we’re doing anyway sign up to our mailing list and follow us on Twitter or Instagram; we have lots of other events taking place between meetings.
