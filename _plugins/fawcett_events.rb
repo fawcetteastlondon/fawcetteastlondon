4fe868baf46a756277dfe1f6d9168e4970f31235
Stephen Tomlinson <Stephen@wcmac31.webcredible.local>
2019-05-24 16:22:40 +0100

    Switched fetching from API to Jekyll plugin

---
+++ _plugins/fawcett_events.rb
@@ -0,0 +1,17 @@
+require 'json'
+require 'open-uri'
+require 'uri'
+
+module Fawwcett_Events
+  class Generator < Jekyll::Generator
+    safe true
+    priority :highest
+
+    def generate(site)
+      headers = { "Authorization" => 'Bearer ' + ENV['EVENTBRITE_TOKEN'] }
+      uri = URI.parse('https://www.eventbriteapi.com/v3/organizations/217989441861/events/')
+      #uri.query = URI.encode_www_form({ :order_by => "start_desc" })
+      site.data['events'] = JSON.load(open(uri, headers))
+    end
+  end
+end
